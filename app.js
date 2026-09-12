8289b16a44cb976fd58956bf9b33afee94cf9fc7
Pasang Tamang <pasanghilp@gmail.com>
2026-09-12 11:51:51 +0545

    add routes for courses and set up server listening

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.get('/',(req,res) => {
+    res.send('say hello to my little friend!!');
+});
+
+app.get('/api/courses',(req,res)=>{
+    res.send([1,2,3]);
+})
+
+app.get('/api/courses/:year/:month', (req,res)=>{
+    res.send(req.params);
+    res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=> console.log(`listening on port ${port}`));
